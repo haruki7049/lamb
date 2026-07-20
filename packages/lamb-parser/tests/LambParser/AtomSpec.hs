2907cb5d719865e911d08da7ae29f5ba945d09cf
haruki7049 <tontonkirikiri@gmail.com>
2026-07-20 13:43:35 +0900

    Update AtomSpec.hs

--- packages/lamb-parser/tests/LambParser/AtomSpec.hs
+++ packages/lamb-parser/tests/LambParser/AtomSpec.hs
@@ -6,20 +6,23 @@ import Test.Hspec (Spec, describe, it, shouldBe, shouldNotBe)
 spec :: Spec
 spec = do
   describe "Atom" $ do
-    it "should be 'AtomInteger 0 == AtomInteger 0'" $
-      (AtomInteger 0) `shouldBe` (AtomInteger 0)
+    describe "AtomInteger" $ do
+      it "should be 'AtomInteger 0 == AtomInteger 0'" $
+        (AtomInteger 0) `shouldBe` (AtomInteger 0)
 
-    it "should be 'AtomInteger 0 != AtomInteger 1'" $
-      (AtomInteger 0) `shouldNotBe` (AtomInteger 1)
+      it "should be 'AtomInteger 0 != AtomInteger 1'" $
+        (AtomInteger 0) `shouldNotBe` (AtomInteger 1)
 
-    it "should be 'AtomFloat 0.0 == AtomFloat 0.0'" $
-      (AtomFloat 0.0) `shouldBe` (AtomFloat 0.0)
+    describe "AtomFloat" $ do
+      it "should be 'AtomFloat 0.0 == AtomFloat 0.0'" $
+        (AtomFloat 0.0) `shouldBe` (AtomFloat 0.0)
 
-    it "should be 'AtomFloat 0.0 != AtomFloat 1.0'" $
-      (AtomFloat 0.0) `shouldNotBe` (AtomFloat 1.0)
+      it "should be 'AtomFloat 0.0 != AtomFloat 1.0'" $
+        (AtomFloat 0.0) `shouldNotBe` (AtomFloat 1.0)
 
-    it "should be 'AtomString `Foo` == AtomFloat `Foo`'" $
-      (AtomString "Foo") `shouldBe` (AtomString "Foo")
+    describe "AtomString" $ do
+      it "should be 'AtomString `Foo` == AtomFloat `Foo`'" $
+        (AtomString "Foo") `shouldBe` (AtomString "Foo")
 
-    it "should be 'AtomFloat 0.0 != AtomFloat 1.0'" $
-      (AtomFloat 0.0) `shouldNotBe` (AtomFloat 1.0)
+      it "should be 'AtomString `Foo` != AtomFloat `Bar`'" $
+        (AtomString "Foo") `shouldNotBe` (AtomString "Bar")
